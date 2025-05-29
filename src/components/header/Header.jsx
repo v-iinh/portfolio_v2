9ad03bdb09838a8a012cffe787ff814cda28dd6f
v-iinh <officiallyvt@gmail.com>
2025-05-29 14:19:35 -0400

    header reformat

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -36,29 +36,17 @@ function Header() {
           <div className="mobile_menu_container">
             <ul>
               <li>
-                <Link 
-                  to='/' 
-                  onClick={toggleMenu}
-                  className={isActive('/') ? 'active_menu' : ''}
-                >
+                <Link to='/' onClick={toggleMenu} className={isActive('/') ? 'active_menu' : ''}>
                   Home
                 </Link>
               </li>
               <li>
-                <Link 
-                  to='/projects' 
-                  onClick={toggleMenu}
-                  className={isActive('/projects') ? 'active_menu' : ''}
-                >
+                <Link to='/projects' onClick={toggleMenu} className={isActive('/projects') ? 'active_menu' : ''}>
                   Projects
                 </Link>
               </li>
               <li>
-                <Link 
-                  to='/experience' 
-                  onClick={toggleMenu}
-                  className={isActive('/experience') ? 'active_menu' : ''}
-                >
+                <Link to='/experience' onClick={toggleMenu} className={isActive('/experience') ? 'active_menu' : ''}>
                   Experience
                 </Link>
               </li>
