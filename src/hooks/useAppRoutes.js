9fc3b2ff4593a15d862728f41e5b3a4921370cd7
v-iinh <officiallyvt@gmail.com>
2025-05-31 15:37:15 -0400

    dynamic routing paths

--- src/hooks/useAppRoutes.js
+++ src/hooks/useAppRoutes.js
@@ -11,10 +11,11 @@ export function useAppRoutes({ isMobile = false, menu, activeMenu, setFullScreen
     return (
         <Routes>
             <Route path='/' element={isMobile ? <Mobile menu={menu} fullScreen={setFullScreen} /> : <Projects menu={menu} activeMenu={activeMenu} />} />
-            <Route path='/experience' element={<Experience menu={menu} activeMenu={activeMenu} />} />
             <Route path='/projects' element={<Projects menu={menu} activeMenu={activeMenu} />} />
+            <Route path='/experience' element={<Experience menu={menu} activeMenu={activeMenu} />} />
             <Route path='/projects/:titleSlug' element={<Expanded menu={menu} activeMenu={activeMenu} />} />
-            <Route path='/experience/:titleSlug' element={<Expanded menu={menu} activeMenu={activeMenu} />} />
+            <Route path='/experience/academic/:titleSlug' element={<Expanded menu={menu} activeMenu={activeMenu} />} />
+            <Route path='/experience/professional/:titleSlug' element={<Expanded menu={menu} activeMenu={activeMenu} />} />
             <Route path='*' element={<Error />} />
         </Routes>
     );
